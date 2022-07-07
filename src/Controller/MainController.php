abccd584f50f11a18337df9c31949454f787e687
MarieLouPLevasseur <sunnymary007@hotmail.com>
2022-07-07 15:05:18 +0200

    ajout route statique liste des films

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,16 +10,35 @@ class MainController extends AbstractController
 {
 
     /**
-     * homepage action
+     *  Show list
      *
      * @Route("/")
      * @return Response
      */
     public function homepage() :Response
     {
+        // dump('controller');
+
         // return new Response('<h1>Homepage</h1>');
         return $this->render('main/homepage.html.twig',[
             'title' =>'Homepage',
+        ]);
+
+    }
+
+
+     /**
+     * Show list
+     *
+     * @Route("/list")
+     * @return Response
+     */
+    public function list() :Response
+    {
+        // dump('controller');
+
+        return $this->render('main/list.html.twig',[
+            'title' =>'Liste des films',
         ]);
 
     }
