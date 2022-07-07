3b51f23512086e20d1428e23924d75a65296c949
MarieLouPLevasseur <sunnymary007@hotmail.com>
2022-07-07 13:57:43 +0200

    installation twig et annotation + affichage homepage

---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+
+    /**
+     * homepage action
+     *
+     * @Route("/")
+     * @return Response
+     */
+    public function homepage() :Response
+    {
+        // return new Response('<h1>Homepage</h1>');
+        return $this->render('main/homepage.html.twig');
+
+    }
+
+}
