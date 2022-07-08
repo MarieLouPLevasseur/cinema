44ee95fb9ca83668ca54b8e467b283986c118666
MarieLouPLevasseur <sunnymary007@hotmail.com>
2022-07-08 12:07:32 +0200

    dynamisation aléatoire films homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,7 @@ class MainController extends AbstractController
 {
 
     /**
-     *  Show list
+     *  Show homepage
      *
      * @Route("/", name="homepage", methods="GET")
      * @return Response
@@ -18,9 +18,37 @@ class MainController extends AbstractController
     public function homepage() :Response
     {
 
+        // préparer les données
+        // ? récupérer les fichiers movies
+        // pour se faire on peut inclure le fichier directement
+        require __DIR__ . '/../../sources/data.php';
+        // on récupère que 2 éléments au hasard du tableau shows
+        // spoiler normalement ce code on le rangerait dans une classe
+        // array_rand 
+
+        //tableau des films
+        // $homepageShows = [];
+        // ? méthode 1 : par sélection aléatoire de deux clés du tableau
+        $randomIndexes = array_rand($shows, 2);
+
+        // permet de récupérer l'index d'un des films aléatoire et d'y stocker les valeurs dans le tableau
+        // random a été défini à 2 index donc le tableau contiendra 2 films
+        foreach ($randomIndexes as $currentRandomIndex)
+        {
+            $homepageShows[$currentRandomIndex] = $shows[$currentRandomIndex];
+        }
+
+        // ? méhode 2: découpe précise du tableau
+
+
+        dump($homepageShows);
+
+
         // return new Response('<h1>Homepage</h1>');
         return $this->render('main/homepage.html.twig',[
             'title' =>'Homepage',
+            'show_list' => $homepageShows
+
         ]);
 
     }
