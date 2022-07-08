627ef06608032b759b939074897f56d6d6b05f7b
MarieLouPLevasseur <sunnymary007@hotmail.com>
2022-07-08 15:08:47 +0200

    dynamisation des favoris

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -38,6 +38,11 @@ class MovieController extends AbstractController
 
         // ! ces données seront utiliser dans méthode "favorite" pour les transmettres à la vue
         // ! cette méthode ne sert qu'a créer le tableau de session et stocker les variables favorites
+        // on ajoute une alerte pour dire que c'est bien ajouté en favoris
+            // success est une clé (dans la boucle nous servira à faire l'affichage que des flash ou warning ou danger (error), etc)
+            // cette "clé" sera utilisé comme "classe" avec l'utilisation du label (voir le template)
+            // cette classe peut etre autre que bootstrap
+        $this->addFlash('success', "Film ajouté aux favoris");
 
         // on redirige l'utilisateur sur la page des favoris
         return $this->redirectToRoute('movie_favorites');
@@ -51,16 +56,39 @@ class MovieController extends AbstractController
      * @Route("/favoris", name="movie_favorites", methods="GET")
      * @return Response
      */
-    public function favorites() :Response
+    public function favorites(Request $request) :Response
     {
 
-        // ! c'est ici qu'on récupère les movies favorites
-
-        session_start();
-        dump($_SESSION);
+        // on démarre une session 
+        // session_start();
+        // dump($_SESSION);
         
+        // ! c'est ici qu'on récupère les movies favorites via la request 
+        // récupération des id de favorite movies depuis la session
+            //récup session
+        $session = $request->getSession();
+            //récup du tableau favoris de la sessions
+        $showIdFavorites = $session->get('show_id_favorites', []);
+        // on place nos données
+
+        require __DIR__ . '/../../sources/data.php';
+        // tableau pour le stockage des films validés: 
+        $favoriteShows = [];
+
+        // on parcours le tableau des films favoris de la session
+            foreach ($showIdFavorites as $currentShowId)
+            {
+                // si le film existe on le met dans le tableau de nos favoris
+                if (isset($shows[$currentShowId]))
+                {
+                    $favoriteShows[$currentShowId] = $shows[$currentShowId];
+                }
+            }
+        // on fournit le tableau à la vue pour dynamiser le template
         return $this->render('main/favorite.html.twig',[
-            'title' =>'Films Favoris',
+            'title' =>'Mes Favoris',
+            'show_list' => $favoriteShows,
+
         ]);
 
     }
